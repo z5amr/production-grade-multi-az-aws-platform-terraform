fb195a7978246768888502425404565bfd33e6fe
z5amr <3mr.a7mdd@gmail.com>
2026-06-29 09:41:48 +0000

    feat: provision VPC and public subnet
    - Enable map_public_ip_on_launch for public subnet

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,11 +1,17 @@
 variable "aws_region" {
-  description = "The AWS region to deploy resources"
+  description = "AWS region for deployment"
   type        = string
   default     = "us-east-1"
 }
 
 variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
+  description = "CIDR block for the main VPC"
   type        = string
   default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR for public subnet"
+  type        = string
+  default     = "10.0.1.0/24"
 }
--- infrastructure/vpc.tf
+++ infrastructure/vpc.tf
@@ -7,3 +7,14 @@ resource "aws_vpc" "main_vpc" {
     Name = "main-vpc"
   }
 }
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = var.public_subnet_cidr
+  availability_zone = "${var.aws_region}a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet"
+  }
+}

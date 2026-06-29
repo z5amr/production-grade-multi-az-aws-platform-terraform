62a693b1fb7a2f716549f25d576abec7478ed3c6
z5amr <3mr.a7mdd@gmail.com>
2026-06-29 07:22:57 +0000

    feat: implement variables for modular infrastructure

---
+++ infrastructure/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "The AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}

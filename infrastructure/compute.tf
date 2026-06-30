48e73393f9e7be2109df0b29b7bc3773570c394c
z5amr <3mr.a7mdd@gmail.com>
2026-06-30 06:56:47 +0000

    feat: add EC2 instance for web server

---
+++ infrastructure/compute.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "web_server" {
+  ami           = "ami-0c101f26f147fa7fd"
+  instance_type = "t3.micro"
+  
+  subnet_id = aws_subnet.public_subnet.id
+
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  tags = {
+    Name = "web-server"
+  }
+}

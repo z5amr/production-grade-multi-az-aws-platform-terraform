0ad255db19b71261011df8596d053c24099f6708
z5amr <3mr.a7mdd@gmail.com>
2026-06-30 08:04:00 +0000

    feat: add user_data script to automate Nginx installation

--- infrastructure/compute.tf
+++ infrastructure/compute.tf
@@ -1,11 +1,17 @@
 resource "aws_instance" "web_server" {
   ami           = "ami-0c101f26f147fa7fd"
   instance_type = "t3.micro"
-  
   subnet_id = aws_subnet.public_subnet.id
-
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
+user_data = <<-EOF
+              #!/bin/bash
+              dnf update -y
+              dnf install -y nginx
+              systemctl start nginx
+              systemctl enable nginx
+              EOF
+              
   tags = {
     Name = "web-server"
   }

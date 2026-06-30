6a2791395c07b7ace14d8c93621e469cb7fb470d
z5amr <3mr.a7mdd@gmail.com>
2026-06-30 09:29:46 +0000

    feat: implement SSH key pair for secure remote access

--- infrastructure/compute.tf
+++ infrastructure/compute.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "web_server" {
   instance_type = "t3.micro"
   subnet_id = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.web_sg.id]
+  key_name = aws_key_pair.web_server_key.key_name
 
 user_data = <<-EOF
               #!/bin/bash
@@ -24,4 +25,9 @@ resource "aws_eip" "web_server_eip" {
   tags = {
     Name = "web-server-eip"
   }
+}
+
+resource "aws_key_pair" "web_server_key" {
+  key_name   = "web-server-key"
+  public_key = file("${path.module}/ssh-keys/id_rsa_webserver.pub")
 }

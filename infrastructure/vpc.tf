2e5a1ded0b9d97181ec6faff49ec57ba54141d60
z5amr <3mr.a7mdd@gmail.com>
2026-06-30 05:26:29 +0000

    feat: add Internet Gateway and public route table

--- infrastructure/vpc.tf
+++ infrastructure/vpc.tf
@@ -18,3 +18,29 @@ resource "aws_subnet" "public_subnet" {
     Name = "public-subnet"
   }
 }
+
+resource "aws_internet_gateway" "main_igw" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main_igw.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}

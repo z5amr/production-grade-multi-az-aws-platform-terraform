f3c772ba1b655c12d80ee8763661e9c15c4cb291
z5amr <3mr.a7mdd@gmail.com>
2026-07-01 03:39:10 +0000

    feat: add Application Load Balancer and integrate with ASG for traffic distribution

---
+++ infrastructure/lb.tf
@@ -0,0 +1,34 @@
+resource "aws_lb" "web_alb" {
+  name               = "web-server-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.web_sg.id]
+  subnets            = [aws_subnet.public_subnet_a.id, aws_subnet.public_subnet_b.id]
+}
+
+resource "aws_lb_target_group" "web_tg" {
+  name     = "web-server-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main_vpc.id
+
+  health_check {
+    path = "/"
+  }
+}
+
+resource "aws_autoscaling_attachment" "asg_attachment" {
+  autoscaling_group_name = aws_autoscaling_group.web_asg.id
+  lb_target_group_arn    = aws_lb_target_group.web_tg.arn
+}
+
+resource "aws_lb_listener" "web_listener" {
+  load_balancer_arn = aws_lb.web_alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web_tg.arn
+  }
+}

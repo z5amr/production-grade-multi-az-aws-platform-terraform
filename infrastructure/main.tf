751a162795083ef4b0ef62a8bdf1547c1a23c62c
z5amr <3mr.a7mdd@gmail.com>
2026-07-03 17:09:25 +0000

    Feat: Add observability layer
    - Implement CloudWatch alarm for ASG High CPU utilization
    
    - Implement CloudWatch alarm for ASG Status Check failures
    
    - Associate alarms with admin-notifications-topic

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -104,3 +104,36 @@ resource "aws_sns_topic_subscription" "email_subscription" {
   protocol  = "email"
   endpoint  = "3mr.a7mdd@gmail.com"
 }
+
+resource "aws_cloudwatch_metric_alarm" "web_cpu_alarm" {
+  alarm_name          = "web-server-high-cpu"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "300" # 5 minutes
+  statistic           = "Average"
+  threshold           = "75"
+  alarm_description   = "This metric monitors ec2 cpu utilization"
+  alarm_actions       = [aws_sns_topic.admin_notifications.arn]
+
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.web_asg.name
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "web_status_check_alarm" {
+  alarm_name          = "web-server-status-check-fail"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "StatusCheckFailed"
+  namespace           = "AWS/EC2"
+  period              = "60"
+  statistic           = "Maximum"
+  threshold           = "0"
+  alarm_actions       = [aws_sns_topic.admin_notifications.arn]
+  
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.web_asg.name
+  }
+}

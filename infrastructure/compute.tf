9540c68131b0a2ea1aeb1976161caf7bedea778c
z5amr <3mr.a7mdd@gmail.com>
2026-07-05 12:19:57 +0000

    Feat: Add automated backups and safety protections to RDS

--- infrastructure/compute.tf
+++ infrastructure/compute.tf
@@ -63,6 +63,9 @@ resource "aws_db_instance" "main_db" {
   storage_encrypted      = true
   db_subnet_group_name   = aws_db_subnet_group.main.name
   vpc_security_group_ids = [aws_security_group.db_sg.id]
+  backup_retention_period = 1
+  backup_window           = "03:00-04:00"
+  deletion_protection     = true
 }
 
 resource "aws_ec2_instance_connect_endpoint" "eice_a" {
--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -2,3 +2,8 @@ output "alb_dns_name" {
   description = "The DNS name of the ALB"
   value       = aws_lb.web_alb.dns_name
 }
+
+output "db_endpoint" {
+  description = "The endpoint of the RDS instance"
+  value       = aws_db_instance.main_db.endpoint
+}
